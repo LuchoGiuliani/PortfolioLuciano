dad123746607573412dec29c2b8067648ed5e7b7
LuchoGiuliani <luciano.giuliani92@gmail.com>
2023-07-23 18:48:06 -0300

    cambios en el title hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,39 @@
-import React from "react";
+import {useEffect, useRef} from "react";
 import styles from "../style";
 import { discount, perfil } from "../assets";
 import GetStarted from "./GetStarted";
+import SplitType from 'split-type'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
-const Hero = () => (
-  <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}   `}>
+const Hero = () => {
+  const refH1 = useRef()
+  const text = new SplitType('#title_h1')
+
+  console.log(text);
+
+  useEffect(() => { 
+  const characters = document.querySelectorAll(".char")
+    gsap.to(characters, { 
+       y:0, 
+       stagger:0.05, 
+       delay: 0.2,   
+       duration: 0.1,
+    })
+  }, [])
+ 
+
+
+return (
+  <section ref={refH1} id="home" className={`flex md:flex-row flex-col ${styles.paddingY}   `}>
      <div className="absolute z-[0] w-[60%] h-[60%] -left-[60%] rounded-full blue__gradient">
-    </div>
-    
-   
-     
+    </div>  
    
     <div
       className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
     >
       <div className="flex flex-row justify-between items-center w-full ">
-        <h1 className="flex-1 font-poppings font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[80px] leading-[75px] sm:text-left text-center">
+        <h1  id="title_h1" className="flex-1 font-poppings font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[80px] leading-[75px] sm:text-left text-center">
           Front-End <br className="sm:block hidden" />{" "}
           <span className="">Developer</span>{" "}
         </h1>
@@ -43,6 +61,6 @@ const Hero = () => (
     </div>
     
   </section>
-);
+)};
 
 export default Hero;
